6ce1198815d20fa106f0038ea61a7a3858c42671
SoCloseToHeaven <halsofods@gmail.com>
2023-03-26 01:25:42 +0300

    lab6 started

---
+++ lab2/query_3.sql
@@ -0,0 +1,12 @@
+SELECT count("Н_ЛЮДИ"."ИД") FROM "Н_ЛЮДИ"
+         WHERE
+            "Н_ЛЮДИ"."ИД" IN (SELECT "Н_УЧЕНИКИ"."ЧЛВК_ИД" FROM "Н_УЧЕНИКИ"
+                                         WHERE "Н_УЧЕНИКИ"."ГРУППА"='3102')
+           AND
+             ('01-01-2011'::date - "Н_ЛЮДИ"."ДАТА_РОЖДЕНИЯ") < '20 years'::interval;
+
+
+/**
+  There is no point to use CURRENT_DATE, because all selected students are older than 20 y.o. at the current moment
+  In that case I have decided to use "01-01-2011" instead of using CURRENT_DATE
+ */
